a31cf094befc3543fc0d69b01765a92267e78eb6
AlfredBryan <alfred.chimereze@gmail.com>
2019-10-22 09:08:15 +0100

    ready for heroku deploy

--- server/server.js
+++ server/server.js
@@ -23,13 +23,13 @@ app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use("/api/v1", postRoute);
 
 const port = process.env.PORT || 4000;
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(port, () => {
